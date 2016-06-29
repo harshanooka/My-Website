e2d54074987591fb66901d7ed068943a716d8042
gowtham-h <hosur.gowtham@gmail.com>
2016-06-29 16:19:02 +0530

    Design fixes

--- js/script.js
+++ js/script.js
@@ -1,24 +1,8 @@
 window.sr = ScrollReveal();
-    sr.reveal('#thumbnail-content-1', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
+    sr.reveal('#thumbnail-content-1');
 
-    sr.reveal('#thumbnail-content-2', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
+    sr.reveal('#thumbnail-content-2');
 
-    sr.reveal('#thumbnail-content-3', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
+    sr.reveal('#thumbnail-content-3');
 
-    sr.reveal('#thumbnail-content-4', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
+    sr.reveal('#thumbnail-content-4');
